17c8599dd3b58ff089ffa6600e0f58ea546d8b69
chajeongheon <jeongheon1127@gmail.com>
2023-12-20 17:36:45 +0900

    123

--- client/src/page/Store/Main/Find/map.js
+++ client/src/page/Store/Main/Find/map.js
@@ -1,33 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { API_URL } from "../../../../config/contansts";
 
 const { kakao } = window;
 
 function Maps() {
+  const [stores, setStores] = useState([]);
+
   useEffect(() => {
-    // 여기에서 Kakao 지도 API를 사용하여 지도를 초기화하는 코드를 작성
-    const container = document.getElementById("map");
-    const options = {
-      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-      level: 3,
+    // 서버에서 매장 정보를 가져오는 비동기 함수
+    const fetchStoreData = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/store`);
+        setStores(response.data);
+         // 이후의 Kakao 지도 코드를 여기에 추가
+        const container = document.getElementById("map");
+        const options = {
+          center: new kakao.maps.LatLng(37.55415495198728, 126.97078657543889),
+          level: 3,
+        };
+        const map = new kakao.maps.Map(container, options);
+
+        // 매장 정보를 이용하여 마커 생성 및 지도에 추가
+        response.data.forEach((store) => {
+          const marker = new kakao.maps.Marker({
+            position: new kakao.maps.LatLng(store.latitude, store.longitude),
+            title: store.name,
+          });
+          marker.setMap(map);
+        });
+
+      } catch (error) {
+        console.error("Error fetching store data:", error.message);
+      }
     };
-    const map = new kakao.maps.Map(container, options);
-
-    //마커를 생성하고 지도 위에 표시
-    const markerPositions = [
-      new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-      new kakao.maps.LatLng(37.360123, 127.105456),
-    ];
-
-     // 각 마커에 대해 반복하여 생성하고 지도 위에 표시
-     markerPositions.forEach((position) => {
-      const marker = new kakao.maps.Marker({
-        position: position,
-      });
-      marker.setMap(map);
-    });
-
-    // 필요한 경우 추가적인 설정 또는 이벤트 처리를 진행
-  }, []);
+    // 매장 정보를 가져온 후 실행
+    fetchStoreData();
+  }, []); // 빈 배열은 컴포넌트가 마운트될 때 한 번만 실행되도록 합니다.
 
   return (
     <div
