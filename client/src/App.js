890e905a3cd60f93b052c5aa95899988b5020ba5
MagnoliaDenudata <sjhj000121@gmail.com>
2023-12-22 03:35:05 +0900

    society

--- client/src/App.js
+++ client/src/App.js
@@ -51,13 +51,43 @@ function App() {
       {!currentPath.includes('/adminmain') &&  <Buttonmain />}
       <Routes>
         <Route path='/' element={<Main></Main>}></Route>
-        <Route path='/test' element={<Test></Test>}></Route>
-        <Route path='/crew' element={<Crew/>}></Route>
-        <Route path='/work' element={<Work/>}></Route>
-        <Route path='/recruit' element={<Recruit/>}></Route>
-        {/* <Route path='/test' element={<Test></Test>}></Route> */}
-        <Route path='/promotion' element={<Promotion />}></Route>
-        <Route path='/promotion/detail/:prodNum' element={<DetailPromotion />}></Route>
+        <Route path='/login' element={<Login />} />
+        <Route path='/join' element={<Join />} />
+        <Route path='/mypage' element={<Mypage />}></Route>
+        {/* <Route element={<Test/>}> */}
+        <Route path='/story/society' element={<Society />} />
+        <Route path='/menu/:subcategory_id' element={<Menu />} />
+        <Route path='/menu/:subcategory_id/:id' element={<DetailProduct />} />
+        <Route path='/story/crew' element={<Crew />}></Route>
+        <Route path='/story/work' element={<Work />}></Route>
+        <Route path='/story/recruit' element={<Recruit />}></Route>
+        <Route path='/story/faq' element={<Faq />}></Route>
+        <Route path='/story/farmtorestaurant' element={<FarmToRestaurant />}></Route>
+        {/* <Route path='/whats-new/happymeal' element={<HappyMeal />}></Route> */}
+        <Route path='/story/brandintro' element={<BrandIntro />}></Route>
+        <Route path='/story/brandhistory' element={<BrandHistory />}></Route>
+        <Route path='/store/find' element={<Find />}></Route>
+        <Route path='/store/event' element={<Event />}></Route>
+        <Route path='/whats-new/13' element={<News />}></Route>
+        <Route path='/whats-new/:subcategory_id' element={<Promotion_HappyMeal />}></Route>
+        <Route path='/whats-new/:page/:prodNum' element={<DetailPage />}></Route>
+        <Route path='/slider' element={<Slider />}></Route>
+        <Route path='/scaleforgood' element={<Scaleforgood />} />
+        <Route path='/kidssoccer' element={<Kidssoccer />} />
+        <Route path='/store/mcdrive' element={<McDrive />}></Route>
+        <Route path='/store/rental' element={<Rental />}></Route>
+        <Route path='/store/mcdelivery' element={<McDelivery />}></Route>
+        <Route path='/story' element={<Storymain />}></Route>
+        <Route path='/private' element={<Private />}></Route>
+        <Route path='/location' element={<Location />}></Route>
+        <Route path='/order' element={<Order />}></Route>
+        <Route path='/happyburger' element={<Happyburger />}></Route>
+        <Route path='/safetyguard' element={<Safetyguard />}></Route>
+        <Route path='/mcdonaldhouse' element={<Mcdonaldhouse />}></Route>
+        <Route path='/mcart' element={<Mcart/>}></Route>
+      
+        <Route path='/adminmain/*' element={<AdminMain />}></Route>
+        {/* </Route> */}
       </Routes>
       {!currentPath.includes('/adminmain') && <Footer />}
     </div>
