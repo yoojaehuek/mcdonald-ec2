ec25339b1ab4d112b70039c0b19043ed00b65556
hoo5576 <hoo5576@naver.com>
2023-12-22 17:26:01 +0900

    21321313213

--- client/src/Admin/page/story/ACrew.js
+++ client/src/Admin/page/story/ACrew.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { API_URL } from '../../../config/contansts';
 
 
 const ACrew = () => {
+  const [axiosResult, setAxiosResult] = useState([]);
+
   useEffect(() => {
-    axios.get(`${API_URL}/product`)
+    axios.get(`${API_URL}/crew`)
     .then(res => {
       console.log(res);
+      setAxiosResult(res.data);
     })
     .catch(err => {
       console.error(err);
@@ -15,7 +18,18 @@ const ACrew = () => {
   }, []);
   return (
     <>
-      <h1>Crew</h1>
+      {axiosResult.map((item, index) => (
+        <div key={index}>
+          <p>ID: {item.id}</p>
+          <p>AdminID: {item.admin_id}</p>
+          <p>StoreID: {item.store_id}</p>
+          <p>타이틀: {item.title}</p>
+          <p>이름: {item.name}</p>
+          <p>이미지: {item.img_url}</p>
+          <p>직무: {item.position}</p>
+          <p>설명: {item.description}</p>
+        </div>
+      ))}
     </>
   );
 };
