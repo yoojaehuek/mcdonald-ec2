561aa943e271ab0a6bf4ac9adb95e94febf7bf94
kimjunnyoung <kimjunnyoung@gmail.com>
2023-12-26 17:38:45 +0900

    0000

--- client/src/page/Story/Competition/FarmToRestaurant/FarmToRestaurant.js
+++ client/src/page/Story/Competition/FarmToRestaurant/FarmToRestaurant.js
@@ -53,7 +53,7 @@ class FarmToRestaurant extends React.Component {
   };
 
   render() {
-    const videoId = 'RZGcvJdRYC0';
+    const videoId = 'vuQt25SUqYg';
 
     const opts = {
       height: '720',  
